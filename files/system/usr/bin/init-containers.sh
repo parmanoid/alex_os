ae8d6fa50b08f8caf236ed1d3b40328d5a7a101e
parmanoid <150517279+parmanoid@users.noreply.github.com>
2026-06-01 00:11:23 +0300

    Refactor init-containers.sh by removing comments
    Removed comments and unnecessary checks from the script.

--- files/system/usr/bin/init-containers.sh
+++ files/system/usr/bin/init-containers.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# Проверяем, запускался ли скрипт раньше
 if [ ! -f ~/.config/containers_created ]; then
-    # Создаем скрытый контейнер Arch Linux для AUR
     distrobox-create --name arch-aur --image archlinux:latest --yes
-    # Создаем скрытый контейнер Ubuntu для DEB
     distrobox-create --name ubuntu-deb --image ubuntu:latest --yes
-    # Создаем файл-маркер, чтобы скрипт больше никогда не запускался
     touch ~/.config/containers_created
 fi
