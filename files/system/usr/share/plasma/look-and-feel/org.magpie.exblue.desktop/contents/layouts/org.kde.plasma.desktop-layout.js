caa93b86375c4744002e64730de9b66fdf79b5cd
parmanoid <150517279+parmanoid@users.noreply.github.com>
2026-06-01 00:02:59 +0300

    Add files via upload

---
+++ files/system/usr/share/plasma/look-and-feel/org.magpie.exblue.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,62 @@
+var panel = new Panel
+// var panelScreen = panel.screen
+panel.location = "bottom";
+panel.height = 38
+panel.alignment = "center";
+panel.lengthMode = "fillavailable"
+panel.floating = true
+
+panel.addWidget("org.kde.plasma.pager")
+panel.addWidget("org.kde.plasma.panelspacer")
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+
+var taskBar = panel.addWidget("org.kde.plasma.icontasks")
+taskBar.currentConfigGroup = ["General"]
+taskBar.writeConfig("launchers",["preferred://browser","preferred://filemanager"])
+
+panel.addWidget("org.kde.plasma.panelspacer")
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
+}
+
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
+
+var dLogout = panel.addWidget("org.kde.plasma.lock_logout");
+dLogout.writeConfig("show_lockScreen", false);
